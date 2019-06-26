f836c5d889085d4535ce807474d89f7eaefb8476
jelly <chm073@gmail.com>
2019-06-26 23:59:35 +0900

    Complete URLs challenge

--- part1/MyPlayground.playground/Contents.swift
+++ part1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
 FileManager.documentDirectoryURL
+
+try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+
+let mysteryDataURL = URL(
+    fileURLWithPath: "mystery",
+    relativeTo: FileManager.documentDirectoryURL
+)
+
+
+let challengeString = "jelly"
+let challengeStringURL = URL(
+    fileURLWithPath: challengeString, relativeTo: FileManager.documentDirectoryURL)
+    .appendingPathExtension("txt")
+
+challengeStringURL.lastPathComponent
