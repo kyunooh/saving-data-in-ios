e1c51f806ff1a5b16f7bbcf6c05df4f76b0da61d
jelly <chm073@gmail.com>
2019-06-27 00:24:08 +0900

    Complete Data

--- part1/MyPlayground.playground/Contents.swift
+++ part1/MyPlayground.playground/Contents.swift
@@ -4,11 +4,24 @@ FileManager.documentDirectoryURL
 
 try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
 
+
+let mysteryBytes: [UInt8] = [
+    240, 240, 159, 184, 185
+]
+
 let mysteryDataURL = URL(
     fileURLWithPath: "mystery",
-    relativeTo: FileManager.documentDirectoryURL
+     relativeTo: FileManager.documentDirectoryURL
 )
 
+let mysteryData = Data(bytes: mysteryBytes);
+try mysteryData.write(to: mysteryDataURL)
+
+let savedMysteryData = try Data(contentsOf: mysteryDataURL)
+let savedMysteryBytes = Array(savedMysteryData);
+
+savedMysteryBytes == mysteryBytes
+mysteryData == savedMysteryData
 
 let challengeString = "jelly"
 let challengeStringURL = URL(
