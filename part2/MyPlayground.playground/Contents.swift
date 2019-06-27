42c6817c4a3720992f4b926b0dce1c777dbc24ae
jelly <chm073@gmail.com>
2019-06-28 00:21:38 +0900

    Complete Json Array Challenge

--- part2/MyPlayground.playground/Contents.swift
+++ part2/MyPlayground.playground/Contents.swift
@@ -22,17 +22,17 @@ struct Image: Decodable {
         self = try decoder.decode(Image.self, from: data)
         
     }
-    func save(directory: FileManager.SearchPathDirectory) {
+    func save(directory: FileManager.SearchPathDirectory) throws  {
         let kindDirectoryURL = URL(
-            fileURLWithPath: kind.rawvalue,
-            relativeTo: FlieManager.default.urls(for: directory, in: .uesrDomainMask)[0]
+            fileURLWithPath: kind.rawValue,
+            relativeTo: FileManager.default.urls(for: directory, in: .userDomainMask)[0]
         )
         
-        try? FileManager.default.createDircetory(at: kindDirectoryURL,
-                                                 withIntermidiateDirectories: true)
+        try? FileManager.default.createDirectory(at: kindDirectoryURL,
+                                                 withIntermediateDirectories: true)
         
         try pngData.write(to: kindDirectoryURL.appendingPathComponent(name).appendingPathExtension("png"),
-        options: .atomic)
+                          options: .atomic)
     }
 }
 
@@ -50,6 +50,47 @@ extension Array where Element == Image {
         
     }
 }
+//
+//let images = try [Image](fileName: "images")
+//images.map { UIImage(data: $0.pngData) }
 
-let images = try [Image](fileName: "images")
-images.map { UIImage(data: $0.pngData) }
+
+
+public struct Sticker: Codable {
+    public let name: String
+    public let size: Int
+    
+    init(name: String, size: Int) {
+        self.name = name
+        self.size = size
+    }
+    
+}
+
+
+
+do {
+    let s1 = Sticker(name: "a", size: 1)
+    let s2 = Sticker(name: "b", size: 2)
+    
+    let stickers: Array<Sticker> = [s1, s2]
+    
+    let jsonEncoder = JSONEncoder()
+    jsonEncoder.outputFormatting = .prettyPrinted
+    let jsonUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        .appendingPathComponent("stickers")
+    
+    let stickersData = try jsonEncoder.encode(stickers)
+    try stickersData.write(to: URL(fileURLWithPath: "stickers",
+                            relativeTo: jsonUrl).appendingPathExtension("json"))
+    
+    let jsonDecoder = JSONDecoder()
+    let savedJSONData = try Data(contentsOf: jsonUrl)
+    let savedSticker = try jsonDecoder.decode([Sticker].self, from: savedJSONData)
+    
+    print(savedSticker)
+} catch  {
+    print(error)
+}
+
+
